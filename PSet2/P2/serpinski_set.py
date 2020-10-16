1f5bed7cd73953bb0319e39d043b5990885f36d4
Ali Mahani <ali.a.mahani@zoho.com>
2020-10-16 19:08:56 +0330

    did some refining. Still fucking inefficient

--- PSet2/P2/serpinski_set.py
+++ PSet2/P2/serpinski_set.py
@@ -10,13 +10,20 @@ from serpinski_class import Serpinski
 def main():
     """Main body of the program"""
     # Initialize the Serpinski set
+    print("==> Making serpinski set...")
     my_serpinski = Serpinski(400, 400, 0)
-    for _ in range(7):
+    num = 8
+    print("==> Generating", num, "levels of subsets :)")
+    for _ in range(9):
         my_serpinski.add_subset()
     # Draw Serpinski
+    print("==> Drawing the set. This might take quite some time!\
+          Damn Inefficient!")
     my_serpinski.draw_me()
     # Show image
-    plt.savefig("fractalstuff.jpg", dpi=500, bbox_inches='tight')
+    dpi = 600
+    print("==> Saving to .jpg with dpi=", dpi)
+    plt.savefig("fractalstuff.jpg", dpi=dpi, bbox_inches='tight')
 
 
 if __name__ == "__main__":
