e6cd8007ec7d96d9e9806510abc34e79894d63e6
Ali Mahani <ali.a.mahani@zoho.com>
2020-10-19 22:04:19 +0330

    Still something wrongs with the code. Gives one every time :-/

--- PSet3/p2/colorize.py
+++ PSet3/p2/colorize.py
@@ -6,17 +6,85 @@ Percolation
 """
 
 import numpy as np
+from matplotlib import pyplot as plt
 from include.generation import gen_rand_sys
 
 
-def colorize():
+def merge(grid, new, old):
+    """ Merge the two clusters """
+    length = grid.shape[0]
+    for i in range(length):
+        for j in range(1, length + 1):
+            if grid[i, j] == old:
+                grid[i, j] = new
+
+
+
+
+def find_cluster(grid, i, j):
+    """ Find the cluster the item belongs to """
+    if i == 0:
+        if grid[i, j - 1] != 0:
+            # Same cluster so same color
+            grid[i, j] = grid[i, j - 1]
+        else:
+            front[0] += 1
+            grid[i, j] = front[0]
+    else:
+        cond_a = grid[i, j - 1] != 0
+        cond_b = grid[i - 1, j] != 0
+        # if cluster around
+        if cond_a or cond_b:
+            a = grid[i, j - 1]
+            b = grid[i - 1, j]
+            # if both are clusters, get the minimum one(sooner created)
+            if cond_a and cond_b:
+                grid[i, j] = min(a, b)
+                # merge the min and max clusters
+                merge(grid, min(a, b), max(a, b))
+            else:
+                # one of them is zero. So color is the other num
+                grid[i, j] = max(a, b)
+        else:
+            front[0] += 1
+            grid[i, j] = front[0]
+
+
+def clusterize(grid):
+    """
+    Find clusters
+    """
+    length = grid.shape[0]
+    global front
+    front = [length + 1]
+    # loop over the cluster
+    for i in range(length):
+        for j in range(1, length + 1):
+            find_cluster(grid, i, j)
+
+
+def colorize(grid):
     """ Colorize the grid """
     # Initialization
-    grid = gen_rand_sys(30)
     length = grid.shape[0]
-    init = np.ones(shape=(length, ))
-    grid_colorize = np.hstack((init, grid))
+    init = np.ones(shape=(length, 1), dtype=int)
+    for i in range(length):
+        init[i, 0] = i + 1
+    grid_color = np.hstack((init, grid))
+    clusterize(grid_color)
+    return grid_color
 
-    for i in range(length + 1):
-        for j in range(length):
-            print(grid_colorize[i, j])
+
+
+def main():
+    """ Main body """
+    # Initialization
+    prob = 0.2
+    grid = gen_rand_sys(30, prob)
+    c_grid = colorize(grid)
+    print(c_grid)
+
+
+
+if __name__ == "__main__":
+    main()
--- PSet3/p2/include/generation.py
+++ PSet3/p2/include/generation.py
@@ -8,17 +8,16 @@ import numpy as np
 from sys import argv
 
 
-def gen_binary_rand():
+def gen_binary_rand(prob):
     """return 1 with probability p, return 0 otherwise"""
-    probability = 0.6
     rand = np.random.uniform()
-    if rand > probability:
+    if rand > prob:
         return 1
     else:
         return 0
 
 
-def randomize_sys(grid):
+def randomize_sys(grid, prob):
     """
     Take a grid and randomize it with probability p
     """
@@ -26,22 +25,23 @@ def randomize_sys(grid):
     for i in range(grid.shape[0]):
         for j in range(grid.shape[1]):
             # Random block assignment
-            grid[i, j] = gen_binary_rand()
+            grid[i, j] = gen_binary_rand(prob)
 
 
-def gen_rand_sys(size):
+def gen_rand_sys(size, prob):
     """
     generate a grid with input size
     """
     # Initialize grid with size
     grid = np.zeros(shape=(size, size), dtype=int, order='F')
     # Randomize grid
-    randomize_sys(grid)
+    randomize_sys(grid, prob)
     # return the generated table
     return grid
 
 def main():
     """ Main body """
+    prob = 0.6
     # validate the input
     if len(argv) != 2:
         print("usage: python gen_rand_sys.py <size of grid>")
@@ -49,7 +49,7 @@ def main():
     else:
         # get size from input
         size = int(argv[1])
-        grid = gen_rand_sys(size)
+        grid = gen_rand_sys(size, prob)
         print(grid)
 
 
