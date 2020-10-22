300adb925f3adec73f958775377e2d7d47835530
Ali Mahani <ali.a.mahani@zoho.com>
2020-10-22 23:26:01 +0330

    Made it work with python. Terrible runtime

--- PSet3/p3/percolation_prob.py
+++ PSet3/p3/percolation_prob.py
@@ -6,27 +6,63 @@ and simulate percolation.
 """
 
 import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+
 from include.generation import gen_rand_sys as grs
 from colorize import colorize, is_percolated
 
 
-def main():
-    """Main body"""
-    size = 10           # The size of our grid
+def iter_prob(size):
+    """ Iterate over the probability and return the Percolation Probability """
+    data = np.zeros(shape=(20, ), dtype=int)
     prob = 0            # probability of a cell being "on"
-    data = np.zeros(shape=(21, ), dtype=int)
 
-    for i in range(21):
+    for i in range(20):
+        prob += 0.05
         perc = 0            # The number of percolations in 100 tries
         for _ in range(100):
             grid = grs(size, prob)
             c_grid = colorize(grid)
             if is_percolated(c_grid):
                 perc += 1
-        prob += 0.05
         data[i] = perc
 
-    print(data)
+    return data
+
+
+
+def main():
+    """Main body"""
+    size = [10, 100, 200]
+    # size = [10]
+    for i in range(len(size)):
+        # LOG
+        print("==> Iterating for size", size[i])
+
+        # Iterate for this size
+        my_data = iter_prob(size[i])
+
+        # Make dataframe and save to csv
+        print("==> Making DataFrame and saving to csv")
+        my_df = pd.DataFrame(data=my_data)
+        my_df.to_csv("my_data" + str(size[i]) + ".csv", index=False)
+
+
+
+
+
+    # x_dummy = np.linspace(0.05, 1, 20)
+    # plt.plot(x_dummy, data, ls='', marker='o')
+    # plt.show()
+
+    # grid = grs(size, 0.5)
+    # c_grid = colorize(grid)
+    # while not is_percolated(c_grid):
+    #     grid = grs(size, 0.5)
+    #     c_grid = colorize(grid)
+    # print(c_grid)
+
 
 
 if __name__ == "__main__":
